d7aa05189db3b5e8208a2037a016e5b8ecd7ceb4
tb0902 <tbruton417@gmail.com>
2024-03-28 13:46:07 -0400

    All types of Pokemon and people have tests.

--- src/test/java/ShelterTest/RapidashStableTest.java
+++ src/test/java/ShelterTest/RapidashStableTest.java
@@ -1,5 +1,51 @@
 package ShelterTest;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class RapidashStableTest {
+  @Test
+  public void stableIsShelter() {
+    RapidashStable stable = new RapidashStable();
+
+    Assert.assertTrue(stable instanceof Shelter);
+  }
+
+  @Test
+  public void addRapidash() {
+    RapidashStable stable = new RapidashStable();
+    Rapidash skipper = new Rapidash();
+
+    stable.add(stable);
+
+    Integer expected = 1;
+    Integer actual = nest.size();
+
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void removeTogekiss() {
+    RapidashStable stable = new RapidashStable();
+    Rapidash blitz = new Rapidash();
+    Rapidash skipper = new Rapidash();
+
+    stable.add(blitz, skipper);
+    stable.remove(blitz);
+
+    Integer expected = 1;
+    Integer actual = stable.size();
+
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void onlyTogekiss() {
+    RapidashStable stable = new RapidashStable();
+    Rapidash blitz = new Rapidash();
+    Bulbasaur bulby = new Bulbasaur();
+
+    stable.add(blitz, bulby);
+
+    Assert.assertThrows(new ClassCastException. class);
+  }
 }
