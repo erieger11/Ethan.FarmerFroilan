24f98e37b4be399d09d7a6217b93e877cc8e844f
Mariyana-Katz <mariqnak21@gmail.com>
2024-03-28 17:10:59 -0400

    some changes

--- src/test/java/CropTest/CropTest.java
+++ src/test/java/CropTest/CropTest.java
@@ -1,5 +1,90 @@
 package CropTest;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CropTest {
+
+    @Test
+    public void testInheritance(){
+        Crop crop = new Crop();
+        Assert.assertTrue(crop instanceof Crop);
+    }
+
+
+
+    @Test
+    public void testImplementation() {
+        Crop crop= new Crop();
+        Assert.assertTrue(crop instanceof Produce);}
+
+        @Test
+     public void testHasBeenFertilized(){
+            CropDuster cropDuster = new CropDuster();
+            CropRow cr = new CropRow(tomatoe, tomatoe, tomatoe);
+            boolean result = cropDuster.hasBeenFertilized();
+            Assert.assertTrue(hasBeenFertilized());
+        }
+
+        @Test
+     public void testHasBeenHarvested(){
+            Tractor tractor = new Tractor();
+            CropRow cr = new CropRow(tomatoe, tomatoe, tomatoe);
+            boolean result = tractor.hasBeenHarvested();
+            Assert.assertTrue( hasBeenHarvested());
+    }
+
+        @Test
+        public void testYield(){
+       int  randomNumber = 7;
+       int yield = 1*randomNumber;
+       Assert.assertEquals(7, yield);
+    }
+
+        @Test
+    public void testYield1(){
+        CropRow<P> cr = new CropRow();
+        if(cr.hasBeenFertilized = true && cr.hasBeenHarvested = false){
+           Assert.assertEquals();
+        }
+        }
+
+
+    @Test
+    public void testYield2(){
+        CropRow<P> cr = new CropRow();
+        if(cr.hasBeenFertilized = false && cr.hasBeenHarvested = true){
+            Assert.assertEquals();
+        }
+    }
+
+    @Test
+    public void testYield3(){
+        CropRow<P> cr = new CropRow();
+
+    }
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/CropTest/TomatoPlantTest.java
+++ src/test/java/CropTest/TomatoPlantTest.java
@@ -1,5 +1,30 @@
 package CropTest;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TomatoPlantTest {
+     @Test
+
+     @Test
+     public void testInheritance(){
+         Tomatoe tomatoe  = new Tomatoe();
+         Assert.assertTrue(tomatoe instanceof Crop);
+     }
+//      @Test
+//
+//    public void TestYieldTomatoe() {
+//         Tomatoe tomatoe  = new Tomatoe();
+//         int yield = tomatoe.yieldTomatoe();
+//         int expected = 30;
+//         Assert.assertEquals(expected, yield);
+
+
+      }
+
+
+
+
+
+
+
 }
