98db6f332d6ff0e6f42287c0658f83d7dc8faa7a
AliceGonzalez <alicegcodes@gmail.com>
2024-03-28 16:09:28 -0400

    Started Testing

--- src/test/java/PersonTest/FarmerTest.java
+++ src/test/java/PersonTest/FarmerTest.java
@@ -1,5 +1,13 @@
 package PersonTest;
+import org.junit.Assert;
+
 import org.junit.Test;
 
 public class FarmerTest {
+    @Test
+    public void testInheritance() {
+        Farmer farmer = new Farmer();
+        Assert.assertTrue(farmer instanceof Farmer);
+    }
 }
+//
--- src/test/java/Vehicles/CropDusterTest.java
+++ src/test/java/Vehicles/CropDusterTest.java
@@ -1,4 +1,26 @@
 package Vehicles;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 public class CropDusterTest  {
+    CropDuster cropDuster;
+
+    @Before
+    public void setup(){
+        cropDuster = new CropDuster();
+    }
+    @Test
+    public void inheritanceTest(){
+        Assert.assertTrue(cropDuster instanceof CropDuster);}
+    @Test
+    public void addPilotTest(){}
+    @Test
+    public void fly(){}
+    @Test
+    public void makeNoiseTest(){}
+    @Test
+    public void addRider(){}
+    @Test
+    public void removeRider(){}
+
 }
