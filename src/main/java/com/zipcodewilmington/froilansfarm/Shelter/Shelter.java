356c9f9c1f6da7de16f4e4b8ad6dcb701e99b8f3
tb0902 <tbruton417@gmail.com>
2024-03-29 18:27:23 -0400

    Changed some more things in Shelter, but should be done.

--- src/main/java/com/zipcodewilmington/froilansfarm/Shelter/Shelter.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Shelter/Shelter.java
@@ -2,14 +2,21 @@ package com.zipcodewilmington.froilansfarm.Shelter;
 
 import com.zipcodewilmington.froilansfarm.Animals.Animal;
 
+import java.util.ArrayList;
+
 public class Shelter<T extends Animal> {
+
+  ArrayList<T> occupants = new ArrayList<>();
+
   public Integer size() {
-    return null;
+    return occupants.size();
   }
 
-  public void add(T bulby) {
+  public void add(T animal) {
+    occupants.add(animal);
   }
 
-  public void remove(T bulby) {
+  public void remove(T animal) {
+    occupants.remove(animal);
   }
 }
