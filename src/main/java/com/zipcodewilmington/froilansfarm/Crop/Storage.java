3d4c89323723fdbb51b764968b135062cbaf3f37
ShailyGH <shailykunal@.com>
2024-03-30 14:10:06 -0400

    made some changes in StorageTest and Storage class.

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Crop/Storage.java
@@ -0,0 +1,17 @@
+package com.zipcodewilmington.froilansfarm.Crop;
+
+import com.zipcodewilmington.froilansfarm.Food.Apple;
+import com.zipcodewilmington.froilansfarm.Food.Edible;
+
+import java.util.HashMap;
+
+public class Storage extends HashMap<Object, Integer>
+{
+   HashMap<Object, Integer> storage = new HashMap();
+
+    public void add(Edible edible, Integer numberofItems)
+    {
+        storage.put(edible, numberofItems);
+    }
+
+}
