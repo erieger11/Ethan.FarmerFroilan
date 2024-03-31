d80fd4509b5db371f101fd19c3588384e14089f7
ShailyGH <shailykunal@.com>
2024-03-30 21:39:44 -0400

    Monday Test.

--- src/main/java/com/zipcodewilmington/froilansfarm/TheInterfaces/Edible_Interface.java
+++
@@ -1,7 +0,0 @@
-package com.zipcodewilmington.froilansfarm.TheInterfaces;
-
-public interface Edible_Interface
-{
-    public Boolean hasBeenHarvested();
-    public Boolean hasBeenFertilized();
-}
--- src/main/java/com/zipcodewilmington/froilansfarm/TheInterfaces/Produce_Interface.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/TheInterfaces/Produce_Interface.java
@@ -2,5 +2,6 @@ package com.zipcodewilmington.froilansfarm.TheInterfaces;
 
 public interface Produce_Interface
 {
-
+    public Boolean hasBeenHarvested();
+   public Boolean hasBeenFertilized();
 }
