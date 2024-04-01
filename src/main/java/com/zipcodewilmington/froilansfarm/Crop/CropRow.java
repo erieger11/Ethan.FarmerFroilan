70aaf595e8ce8c044d033f72d431341b94060783
erieger11 <erieger530@gmail.com>
2024-03-31 20:24:54 -0400

    added some more tests

--- src/main/java/com/zipcodewilmington/froilansfarm/Crop/CropRow.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Crop/CropRow.java
@@ -1,17 +1,65 @@
 package com.zipcodewilmington.froilansfarm.Crop;
 
-public class CropRow {
-    public boolean hasBeenFertalized;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Boolean beenHarvested() {
-        return null;
+public class CropRow<T extends Crop> {
+    List<T> cropRow;
+    private boolean hasRowWatered = false;
+    private  boolean hasRowFertilized = false;
+    private boolean hasRowHarvested = false;
+
+    public CropRow(){
+        this.cropRow = new ArrayList<>();
     }
 
-    public boolean beenFertalized() {
-        return false;
+    public CropRow( ArrayList<T> newRow){
+        this.cropRow = newRow;
     }
 
-    public boolean hasBeenNurtured;
-    public boolean hasBeenBurned;
+    public void addCrop(T crop) {
+        this.cropRow.add(crop);
+
+    }
+
+    public int getCropRowSize() {
+        return this.cropRow.size();
+    }
+
+    public boolean isRowEmpty() {
+        return cropRow.isEmpty();
+    }
+
+    public boolean hasBeenFertilized() {
+        return hasRowFertilized;
+    }
+
+    public void fertilizeRow(CropRow cr) {
+//        for(T crop: this.cropRow){
+//            crop.fertilize();
+        cr.hasRowFertilized = true;
+//        }
+        // isRowFertilized =true;
+    }
+
+    public boolean hasBeenHarvested() {
+        return hasRowHarvested;
+    }
+
+    public void harvestRow(CropRow cr) {
+//        for(T crop: this.cropRow){
+//            crop.harvest();
+        cr.hasRowHarvested = true;
+//        }
+//        isRowHarvested =true;
+    }
+
+    public boolean hasBeenWatered() {
+        return hasRowWatered;
+    }
+
+    public void waterRow(CropRow cr) {
+        cr.hasRowWatered = true;
+    }
 
 }
--- src/test/java/GenericsTest/CropRowTest.java
+++ src/test/java/GenericsTest/CropRowTest.java
@@ -1,62 +1,208 @@
 package GenericsTest;
+import CropTest.TomatoPlantTest;
+import PersonTest.FarmerTest;
+import com.zipcodewilmington.froilansfarm.Animals.Squirtle;
+import com.zipcodewilmington.froilansfarm.Crop.Crop;
+import com.zipcodewilmington.froilansfarm.Crop.TomatoPlant;
 import com.zipcodewilmington.froilansfarm.Crop.CropRow;
+import com.zipcodewilmington.froilansfarm.Food.Tomato;
+import com.zipcodewilmington.froilansfarm.Person.Farmer;
+import com.zipcodewilmington.froilansfarm.Vehicle.CropDuster;
+import com.zipcodewilmington.froilansfarm.Vehicle.Tractor;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
 public class CropRowTest {
-    @Test
+
 
     public void testCropRow(){
-        CropRow croprow = new CropRow();
+        CropRow<Crop> croprow = new CropRow<Crop>();
         Assert.assertTrue(croprow instanceof Crop);
     }
-     @Test
+
+    @Test
+    public void testHasBeenPlanted(){
+        CropRow<TomatoPlant> rowOne = new CropRow<>();
+        Farmer froilan = new Farmer();
+        TomatoPlant tPlant = new TomatoPlant();
+
+        froilan.plant(tPlant, rowOne);
+
+        Assert.assertTrue(rowOne.hasBeenPlanted);
+    }
+//
+//     @Test
+//     public void testGetHowManyPlanted(){
+//         CropRow<TomatoPlant> rowOne = new CropRow<>();
+//         TomatoPlant tPlant = new TomatoPlant();
+//         Farmer froilan = new Farmer();
+//
+//         froilan.plant(tPlant, rowOne);
+//         froilan.plant(tPlant, rowOne);
+//         froilan.plant(tPlant, rowOne);
+//         froilan.plant(tPlant, rowOne);
+//         froilan.plant(tPlant, rowOne);
+//
+//         Integer expected = 5;
+//         Integer actual = rowOne.size();
+//
+//         Assert.assertEquals(actual, expected);
+//     }
+
+    @Test
+    public void testBeenFertilized(){
+        CropRow<TomatoPlant> rowOne = new CropRow<>();
+        //TomatoPlant tPlant = new TomatoPlant();
+        CropDuster cropDuster = new CropDuster();
+
+        cropDuster.fertilize(rowOne);
+
+        Assert.assertTrue(rowOne.hasBeenFertilized());
+    }
+
+    @Test
     public void testAddCrop(){
-//        CropRow cropRow = new CropRow();
-//        Crop crop = new crop();
-//        CropRow.addCrop(expected);
-//        Crop actual = cropRow.getRow()<>;
-//        Assert.assertEquals(actual, expected);
+        CropRow<TomatoPlant> rowOne = new CropRow<>();
+        Farmer froilan = new Farmer();
+        TomatoPlant tPlant = new TomatoPlant();
+
+        froilan.plant(tPlant, rowOne);
+
+        Assert.assertTrue(rowOne.contains(tPlant));
+
+    }
+
+    @Test
+    public void testBeenHarvested(){
+        CropRow<TomatoPlant> rowOne = new CropRow<>();
+        TomatoPlant tPlant = new TomatoPlant();
+        Tractor tractor = new Tractor();
+
+        tractor.harvest(rowOne);
+
+        Assert.assertTrue(rowOne.hasBeenHarvested());
+    }
+
+
+    @Test
+    public void testWasWatered(){
+        CropRow<TomatoPlant> rowOne = new CropRow<>();
+        TomatoPlant tPlant = new TomatoPlant();
+        Squirtle squirtle = new Squirtle();
+
+        squirtle.water(rowOne);
+
+        Assert.assertTrue(rowOne.hasBeenWatered());
+    }
+
+
+    @Test
+    public void testisRowEmpty(){
+        CropRow<TomatoPlant> rowOne = new CropRow<>();
+        TomatoPlant tPlant = new TomatoPlant();
+
+        rowOne.clear();
+
+        Assert.assertTrue(rowOne.isRowEmpty());
+//Hello
+
+    }
+
+//     @Test
+//    public void testAddCrop(){
+//        CropRow<TomatoPlant> cropRow = new CropRow<>();
+//        TomatoPlant tomatoPlant = new TomatoPlant();
+//        cropRow.addCrop(tomatoPlant);
+//        int actual =  cropRow.getCropRowSize();
+//        int expected = 1;
+//        Assert.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void testGetCropRowSize(){
+//        CropRow<TomatoPlant> cropRow = new CropRow<>();
+//        TomatoPlant tomatoPlant = new TomatoPlant();
+//        cropRow.addCrop(tomatoPlant);
+//        int actual =  cropRow.getCropRowSize();
+//        int expected = 1;
+//        Assert.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void testRowEmpty(){
+//        CropRow<TomatoPlant> cropRow = new CropRow<>();
+//        Assert.assertTrue(cropRow.isRowEmpty());
+//
+//    }
+//
+//    @Test
+//    public void testFertilize(){
+//        CropRow<TomatoPlant> cropRow = new CropRow<>();
+//        Assert.assertFalse(cropRow.hasBeenFertilized());
+//
+//        cropRow.fertilizeRow();
+//        Assert.assertTrue(cropRow.hasBeenFertilized());
+//    }
+//
+//
+//    @Test
+//    public void testHasBeenFertilized() {
+//        CropRow<TomatoPlant> cropRow = new CropRow<>();
+//        Assert.assertFalse(cropRow.hasBeenFertilized());
+//
+//        cropRow.fertilizeRow();
+//        Assert.assertTrue(cropRow.hasBeenFertilized());
+//    }
+//
+//
+//    @Test
+//    public void testHarvestRow(){
+//        CropRow<TomatoPlant> cropRow = new CropRow<>();
+//        Assert.assertFalse(cropRow.hasBeenHarvested());
+//
+//        cropRow.harvestRow();
+//        Assert.assertTrue(cropRow.hasBeenHarvested());
+//    }
+//
+//
+//    @Test
+//    public void testHasBeenHarvested() {
+//        CropRow<TomatoPlant> cropRow = new CropRow<>();
+//        Assert.assertFalse(cropRow.hasBeenHarvested());
+//
+//        cropRow.harvestRow();
+//        Assert.assertTrue(cropRow.hasBeenHarvested());
+//    }
+//
+//    @Test
+//    public void testWaterRow(){
+//        CropRow<TomatoPlant> cropRow = new CropRow<>();
+//        Assert.assertFalse(cropRow.hasBeenWatered());
+//
+//        cropRow.waterRow();
+//        Assert.assertTrue(cropRow.hasBeenWatered());
+//    }
+//
+//
+//    @Test
+//    public void testHasBeenWatered() {
+//        CropRow<TomatoPlant> cropRow = new CropRow<>();
+//        Assert.assertFalse(cropRow.hasBeenWatered());
+//
+//        cropRow.waterRow();
+//        Assert.assertTrue(cropRow.hasBeenWatered());
+//    }
 //
 
-     }
-     @Test
-     public void testGetPlanted(){
-
-     }
-
-     @Test
-    public void testBeenFertalized(){
-
-     }
-
-     @Test
-    public void testBeenPlanted(){
-
-     }
-
-     @Test
-    public void testBeenHarvested(){
-
-     }
-
-     @Test
-    public void testFertilizedRow(){
-
-     }
-
-     @Test
-    public void testHarvestRow(){
-
-     }
-
-     @Test
-    public void testRowEmpty(){
-
-     }
-
-
-
+//    @Test
+//    public void testYieldProduce() {
+//        CropRow<TomatoPlant> cropRow1 = new CropRow<>();
+//        TomatoPlant tomatoPlant = new TomatoPlant();
+//        tomatoPlant.addCrop(tomatoPlant);
+//        cropRow1.fertilizeRow();
+//        Assert.assertTrue(cropRow1.yieldProduce() instanceof Tomato);
+//
+//    }
 }
